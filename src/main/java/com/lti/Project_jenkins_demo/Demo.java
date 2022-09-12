96dfb88805bcdc0a92e6e98da0fdba20cd5c8f33
Pranav <pranavjagtap72@gmail.com>
2022-09-12 15:58:55 +0530

    commited

---
+++ src/main/java/com/lti/Project_jenkins_demo/Demo.java
@@ -0,0 +1,13 @@
+package com.lti.Project_jenkins_demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i<10; i++) {
+			System.out.println("Demo with jenkins");
+		}
+	}
+
+}
